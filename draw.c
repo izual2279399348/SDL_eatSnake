ee12c9d7da35a656b9d36df5250b11520fca0f3c
izual2279399348 <45808802+izual2279399348@users.noreply.github.com>
2019-04-22 10:30:21 +0800

    Add files via upload

---
+++ draw.c
@@ -0,0 +1,100 @@
+#include "SDL2/SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 720
+
+const SDL_Color BLACK = { .r = 0, .g = 0, .b = 0, .a = 255 };
+const SDL_Color WHITE = { .r = 255, .g = 255, .b = 255, .a = 255 };
+
+const SDL_Color RED = { .r = 255, .g = 0, .b = 0, .a = 255 };
+const SDL_Color MAROON =  { .r = 128, .g = 0, .b = 0, .a = 255 };
+
+const SDL_Color BLUE = { .r = 0, .g = 0, .b = 255, .a = 255 };
+const SDL_Color NAVY = { .r = 0, .g = 0, .b = 128, .a = 255 };
+
+const SDL_Color LIME = { .r = 0, .g = 255, .b = 0, .a = 255 };
+const SDL_Color GREEN = { .r = 0, .g = 128, .b = 0, .a = 255 };
+
+// 4:3 aspect ratio is assumed
+//
+// FIELD_CELL_WIDTH has to divisible by 4, will have to divide SCREEN_WIDTH
+#define FIELD_CELL_WIDTH 32
+#define CELL_SIZE ((SCREEN_WIDTH) / (FIELD_CELL_WIDTH))
+#define FIELD_CELL_HEIGHT ((FIELD_CELL_WIDTH) / 4 * 3)
+
+#define FIELD_START_CELL_X 0
+#define FIELD_START_CELL_Y 0
+
+void draw_field(SDL_Renderer *renderer){
+    SDL_Color prev;
+    SDL_GetRenderDrawColor(renderer, &prev.r, &prev.g, &prev.b, &prev.a);
+
+    SDL_SetRenderDrawColor(renderer, NAVY.r, NAVY.g, NAVY.b, NAVY.a);
+    SDL_Rect field = {
+        FIELD_START_CELL_X * CELL_SIZE,
+        FIELD_START_CELL_Y * CELL_SIZE,
+        FIELD_CELL_WIDTH * CELL_SIZE,
+        FIELD_CELL_HEIGHT * CELL_SIZE
+    };
+
+    SDL_RenderFillRect(renderer, &field);
+    SDL_SetRenderDrawColor(renderer, BLUE.r, BLUE.g, BLUE.b, BLUE.a);
+    for (int i = 1; i < FIELD_CELL_WIDTH; i++) {
+        SDL_RenderDrawLine(renderer, i * CELL_SIZE, 0,
+                i * CELL_SIZE, FIELD_CELL_HEIGHT * CELL_SIZE);
+    }
+    for (int i = 1; i < FIELD_CELL_HEIGHT; i++) {
+        SDL_RenderDrawLine(renderer, 0, i * CELL_SIZE,
+                FIELD_CELL_WIDTH * CELL_SIZE, i * CELL_SIZE);
+    }
+
+    SDL_SetRenderDrawColor(renderer, prev.a, prev.g, prev.b, prev.a);
+}
+
+void draw_food(SDL_Renderer *renderer){
+    SDL_Color prev;
+    SDL_GetRenderDrawColor(renderer, &prev.r, &prev.g, &prev.b, &prev.a);
+    SDL_SetRenderDrawColor(renderer, RED.r, RED.g, RED.b, RED.a);
+
+    SDL_Rect food_cell = {
+        .x = theFood.c * CELL_SIZE, .y = theFood.r * CELL_SIZE,
+        .w = CELL_SIZE, .h = CELL_SIZE
+    };
+    SDL_RenderFillRect(renderer, &food_cell);
+    SDL_SetRenderDrawColor(renderer, MAROON.r, MAROON.g, MAROON.b,
+            MAROON.a);
+    SDL_RenderDrawRect(renderer, &food_cell);
+    SDL_SetRenderDrawColor(renderer, prev.r, prev.g, prev.b, prev.a);
+}
+
+
+void draw_snake(SDL_Renderer *renderer){
+    SDL_Color prev;
+    SDL_GetRenderDrawColor(renderer, &prev.a, &prev.g, &prev.b, &prev.a);
+    for (int i = 0; i < snake.size; i++) {
+        SDL_Rect cell = { snake.segments[i].c * CELL_SIZE,
+            snake.segments[i].r * CELL_SIZE,
+            CELL_SIZE, CELL_SIZE
+        };
+
+        SDL_SetRenderDrawColor(renderer, LIME.r, LIME.g, LIME.b, LIME.a);
+        SDL_RenderFillRect(renderer, &cell);
+
+        SDL_SetRenderDrawColor(renderer,GREEN.r,GREEN.g,GREEN.b, 255);
+        SDL_RenderDrawRect(renderer, &cell);
+    }
+
+    SDL_SetRenderDrawColor(renderer, prev.a, prev.g, prev.b, prev.a);
+}
+
+void draw_screen(SDL_Renderer *renderer){
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    draw_field(renderer);
+    draw_apple(renderer);
+    draw_snake(renderer);
+}
